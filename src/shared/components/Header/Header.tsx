1607de5a996809747adef04f7e05151419ac92dc
marcebenitez2 <marcebenitez0607@gmail.com>
2024-07-20 02:05:45 -0300

    feat: sidebar, header

---
+++ src/shared/components/Header/Header.tsx
@@ -0,0 +1,31 @@
+"use client";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { Boxes } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { ThemeToggle } from "./ThemeToggle";
+import { MobileSidebar } from "../Sidebar/Mobile-Sidebar";
+
+export default function Header() {
+  return (
+    <div className="supports-backdrop-blur:bg-background/60 fixed left-0 right-0 top-0 z-20 border-b bg-background/95 backdrop-blur">
+      <nav className="flex h-16 items-center justify-between px-4">
+        <Link
+          href={"/"}
+          className="hidden items-center justify-between gap-2 md:flex"
+        >
+          <Boxes className="h-6 w-6" />
+          <h1 className="text-lg font-semibold">Uesevi</h1>
+        </Link>
+        <div className={cn("block md:!hidden")}>
+          <MobileSidebar />
+        </div>
+
+        <div className="flex items-center gap-2">
+          <ThemeToggle />
+          <Button size="sm">Cerrar sesion</Button>
+        </div>
+      </nav>
+    </div>
+  );
+}
---
+++ src/shared/constants/side-nav.ts
@@ -0,0 +1,126 @@
+import {
+  LayoutDashboard,
+  Users,
+  UserRoundCog,
+  Building2,
+  Pickaxe,
+  Newspaper,
+  FileText,
+  BookUser,
+  Percent,
+  BookText,
+  BookMarked,
+  Scroll,
+  Search,
+  BookCopy,
+} from "lucide-react";
+import { type LucideIcon } from "lucide-react";
+
+export interface NavItem {
+  title: string;
+  href: string;
+  icon: LucideIcon;
+  color?: string;
+  isChidren?: boolean;
+  children?: NavItem[];
+}
+
+export const NavItems: NavItem[] = [
+  {
+    title: "Dashboard",
+    icon: LayoutDashboard,
+    href: "/",
+    color: "text-sky-500",
+  },
+  {
+    title: "Usuarios",
+    icon: Users,
+    href: "/usarios",
+    color: "text-rose-500",
+    isChidren: true,
+    children: [
+      {
+        title: "Administradores",
+        href: "/usuarios/admins",
+        icon: UserRoundCog,
+        color: "text-rose-500",
+      },
+      {
+        title: "Empresas",
+        href: "/usuarios/empresas",
+        icon: Building2,
+        color: "text-rose-500",
+      },
+
+      {
+        title: "Empleados",
+        href: "/usuarios/empleados",
+        icon: Pickaxe,
+        color: "text-rose-500",
+      },
+      {
+        title: "Busqueda por empresa",
+        href: "/usuarios/busqueda",
+        icon: Search,
+        color: "text-rose-500",
+      },
+      {
+        title: "Formulario",
+        href: "/usuarios/formulario",
+        icon: BookCopy,
+        color: "text-rose-500",
+      },
+    ],
+  },
+  // Ahora noticias, cambiando el color del icono
+  {
+    title: "Noticias",
+    icon: Newspaper,
+    href: "/noticias",
+    color: "text-green-500",
+  },
+  //Ahora escalas, cambiando el color del icono
+  {
+    title: "Escalas",
+    icon: FileText,
+    href: "/escalas",
+    color: "text-yellow-500",
+  },
+
+  //Ahora categorias
+  {
+    title: "Categorias",
+    icon: BookUser,
+    href: "/categorias",
+    color: "text-blue-500",
+  },
+  // Otro color
+  {
+    title: "Tasa de interes",
+    icon: Percent,
+    href: "/interes",
+    color: "text-red-500",
+  },
+  {
+    title: "Declaraciones juradas",
+    icon: BookText,
+    href: "/declaraciones",
+    color: "text-indigo-500",
+    isChidren: true,
+    children: [
+      // Una para declaraciones juradas y otra para declaraciones juradas antiguas
+      {
+        title: "Declaraciones juradas",
+        href: "/declaraciones/declaraciones",
+        icon: Scroll,
+        color: "text-indigo-500",
+      },
+      {
+        title: "Declaraciones antiguas",
+        href: "/declaraciones/antiguas",
+        icon: BookMarked,
+        color: "text-indigo-500",
+      },
+    ],
+  },
+];
