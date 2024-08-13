d8cfde3272242e0f21609ab3a7c15f9ead232c96
mateoBodiniARG <bodinidev@gmail.com>
2024-08-13 14:45:12 -0300

    feat: se quitó el h1 al lado del menuIcon cuando es responsive

--- src/shared/components/Sidebar/Mobile-Sidebar.tsx
+++ src/shared/components/Sidebar/Mobile-Sidebar.tsx
@@ -5,32 +5,31 @@ import { SideNav } from "./Side-Nav";
 import { NavItems } from "@/shared/constants/side-nav";
 
 export const MobileSidebar = () => {
-    const [open, setOpen] = useState(false);
-    const [isMounted, setIsMounted] = useState(false);
+  const [open, setOpen] = useState(false);
+  const [isMounted, setIsMounted] = useState(false);
 
-    useEffect(() => {
-        setIsMounted(true);
-    }, []);
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
 
-    if (!isMounted) {
-        return null;
-    }
+  if (!isMounted) {
+    return null;
+  }
 
-    return (
-        <>
-            <Sheet open={open} onOpenChange={setOpen}>
-                <SheetTrigger asChild>
-                    <div className="flex items-center justify-center gap-2">
-                        <MenuIcon />
-                        <h1 className="text-lg font-semibold">T3 app template</h1>
-                    </div>
-                </SheetTrigger>
-                <SheetContent side="left" className="w-72">
-                    <div className="px-1 py-6 pt-16">
-                        <SideNav items={NavItems} setOpen={setOpen} />
-                    </div>
-                </SheetContent>
-            </Sheet>
-        </>
-    );
+  return (
+    <>
+      <Sheet open={open} onOpenChange={setOpen}>
+        <SheetTrigger asChild>
+          <div className="flex items-center justify-center gap-2">
+            <MenuIcon />
+          </div>
+        </SheetTrigger>
+        <SheetContent side="left" className="w-72">
+          <div className="px-1 py-6 pt-16">
+            <SideNav items={NavItems} setOpen={setOpen} />
+          </div>
+        </SheetContent>
+      </Sheet>
+    </>
+  );
 };
