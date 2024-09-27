ea6fa2ec50e826160f4e6a83c6cf2b124fca92c0
mateoBodiniARG <bodinidev@gmail.com>
2024-09-27 17:33:15 -0300

    feat: cambios en el loginEmpresa

--- src/modules/Client/loginEmpresa/index.tsx
+++ src/modules/Client/loginEmpresa/index.tsx
@@ -17,7 +17,6 @@ import { postData } from "@/services/mysql/functions";
 import { userStore } from "@/shared/stores/userStore";
 import { setCookie } from "cookies-next";
 import { useRouter } from "next/navigation";
-import { FramerComponent } from "@/shared/Framer/FramerComponent";
 import LoadingSpinner from "@/modules/Login/components/LoadingSpinner";
 
 export function LoginEmpresaModule() {
@@ -38,14 +37,17 @@ export function LoginEmpresaModule() {
       if (data.error) {
         toast.error(result.data.details.message);
       }
+      console.log(data);
       const { user, token } = data;
 
       userStore.getState().setAuth(token, user);
       setCookie("auth-token", token);
 
+      console.log("Usuario autenticado", user);
+
       router.replace("/empresa/dashboard");
     } catch (error) {
-      console.log(error);
+      console.error(error);
       toast.error("Ocurrió un error al iniciar sesión");
       setLoading(false);
     }
@@ -53,53 +55,53 @@ export function LoginEmpresaModule() {
 
   return (
     <div className="min-h-screen flex items-center justify-center p-4">
-      <FramerComponent
-        animationInitial={{ y: 100, opacity: 0 }}
-        animationAnimate={{ y: 0, opacity: 1 }}
-      >
-        <Card className="w-full max-w-md">
-          <CardHeader className="space-y-1">
-            <div className="flex items-center justify-center mb-4">
-              <Building2 className="h-12 w-12 text-blue-600" />
-            </div>
-            <CardTitle className="text-2xl font-bold text-center">
-              Inicio de Sesión Empresarial
-            </CardTitle>
-            <CardDescription className="text-center">
-              Ingrese sus credenciales para acceder al sistema
-            </CardDescription>
-          </CardHeader>
-          <CardContent className="space-y-4">
-            <div className="space-y-2">
-              <Label htmlFor="email">Correo electrónico</Label>
-              <Input
-                id="email"
-                placeholder="nombre@empresa.com"
-                required
-                type="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </div>
-            <div className="space-y-2">
-              <Label htmlFor="password">Contraseña</Label>
-              <Input
-                id="password"
-                required
-                type="password"
-                placeholder="********"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </div>
-          </CardContent>
-          <CardFooter>
-            <Button className="w-full" onClick={handleSubmit}>
-              Iniciar sesión
-            </Button>
-          </CardFooter>
-        </Card>
-      </FramerComponent>
+      <Card className="w-full max-w-md">
+        <CardHeader className="space-y-1">
+          <div className="flex items-center justify-center mb-4">
+            <Building2 className="h-12 w-12 text-blue-600" />
+          </div>
+          <CardTitle className="text-2xl font-bold text-center">
+            Inicio de Sesión Empresarial
+          </CardTitle>
+          <CardDescription className="text-center">
+            Ingrese sus credenciales para acceder al sistema
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="space-y-4">
+          <div className="space-y-2">
+            <Label htmlFor="email">Correo electrónico</Label>
+            <Input
+              id="email"
+              placeholder="nombre@empresa.com"
+              required
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="space-y-2">
+            <Label htmlFor="password">Contraseña</Label>
+            <Input
+              id="password"
+              required
+              type="password"
+              placeholder="********"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+        </CardContent>
+        <CardFooter>
+          <Button
+            onClick={handleSubmit}
+            type="submit"
+            className="w-full"
+            disabled={loading}
+          >
+            {loading ? <LoadingSpinner /> : "Iniciar sesión"}
+          </Button>
+        </CardFooter>
+      </Card>
     </div>
   );
 }
