e3f712061ceedb05555fa1834ccf88f940e5e798
marcebenitez2 <marcebenitez0607@gmail.com>
2024-10-02 11:35:56 -0300

    feat: manejo de error en la declaracion vieja

--- src/app/admin/(sidebar)/antiguas/[idEmpresa]/[idDeclaracion]/page.tsx
+++ src/app/admin/(sidebar)/antiguas/[idEmpresa]/[idDeclaracion]/page.tsx
@@ -1,14 +1,21 @@
 import { DeclaracionModule } from "@/modules/Admin/Antiguas/DeclaracionById";
 import { fetchData } from "@/services/mysql/functions";
 
-
 export default async function Declaracion({
   params: { idEmpresa, idDeclaracion },
 }: {
   params: { idEmpresa: number; idDeclaracion: number };
 }) {
-  const statement = await fetchData(
-    `old-statements/getInfo/${idEmpresa}/${idDeclaracion}`
+  const statementResult = await fetchData(
+    `old-statements/info/${idEmpresa}/${idDeclaracion}`
   );
+  console.log(statementResult);
+
+  if (!statementResult.ok) {
+    return <div>Error al cargar los datos</div>;
+  }
+
+  const statement = statementResult.data;
+
   return <DeclaracionModule statement={statement} />;
 }
