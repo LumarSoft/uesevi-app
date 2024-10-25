d1039252bb4afad5ec15f5fe79c8e530df9e6108
mateoBodiniARG <bodinidev@gmail.com>
2024-10-25 14:33:03 -0300

    feat: se cambio la imagen del hero y además se dio funcionalidad a la flecha de ultimas noticias

--- src/modules/Client/Home/components/LatestNews/LatestNews.tsx
+++ src/modules/Client/Home/components/LatestNews/LatestNews.tsx
@@ -35,41 +35,43 @@ const LatestNews = ({ noticias }: { noticias: INoticias[] }) => {
   if (!noticias) return null;
 
   return (
-    <FramerComponent
-      style="lg:h-screen bg-muted flex justify-center items-center py-16"
-      animationInitial={{ opacity: 0, y: 50 }}
-      animationWhileInView={{ opacity: 1, y: 0 }}
-      animationTransition={{ duration: 0.3, delay: 0.1 }}
-      animationViewPort={{ once: true, offset: 0.4 }}
-    >
-      <div className="container mx-auto px-4">
-        <h2 className="text-3xl font-bold mb-8 text-center text-primary">
-          Ultimas noticias
-        </h2>
-        <FramerComponent
-          style="grid grid-cols-1 md:grid-cols-3 gap-8 mb-8"
-          animationVariants={container}
-          animationInitial="hidden"
-          animationWhileInView="show"
-          animationViewPort={{ once: true, offset: 0.4 }}
-        >
-          {noticias.map((noticia, i) => (
-            <FramerComponent
-              key={i}
-              animationInitial={{ opacity: 0, y: 50 }}
-              animationVariants={itemAnimado}
-            >
-              <CardNoticias noticia={noticia} />
-            </FramerComponent>
-          ))}
-        </FramerComponent>
-        <div className="text-center">
-          <Link href="/noticias/page/1">
-            <Button>Ver mas noticias</Button>
-          </Link>
+    <div id="lastestNews">
+      <FramerComponent
+        style="lg:h-screen bg-muted flex justify-center items-center py-16"
+        animationInitial={{ opacity: 0, y: 50 }}
+        animationWhileInView={{ opacity: 1, y: 0 }}
+        animationTransition={{ duration: 0.3, delay: 0.1 }}
+        animationViewPort={{ once: true, offset: 0.4 }}
+      >
+        <div className="container mx-auto px-4">
+          <h2 className="text-3xl font-bold mb-8 text-center text-primary">
+            Ultimas noticias
+          </h2>
+          <FramerComponent
+            style="grid grid-cols-1 md:grid-cols-3 gap-8 mb-8"
+            animationVariants={container}
+            animationInitial="hidden"
+            animationWhileInView="show"
+            animationViewPort={{ once: true, offset: 0.4 }}
+          >
+            {noticias.map((noticia, i) => (
+              <FramerComponent
+                key={i}
+                animationInitial={{ opacity: 0, y: 50 }}
+                animationVariants={itemAnimado}
+              >
+                <CardNoticias noticia={noticia} />
+              </FramerComponent>
+            ))}
+          </FramerComponent>
+          <div className="text-center">
+            <Link href="/noticias/page/1">
+              <Button>Ver mas noticias</Button>
+            </Link>
+          </div>
         </div>
-      </div>
-    </FramerComponent>
+      </FramerComponent>
+    </div>
   );
 };
 
