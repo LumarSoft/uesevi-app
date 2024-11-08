8ab56cc59c149391eae6f46d7c18d5a1f8c428d9
marcebenitez2 <marcebenitez0607@gmail.com>
2024-11-08 14:58:42 -0300

    feat: las empresas ya no pueden achicar la sidebar

--- src/shared/components/SidebarCompany/Sidebar.tsx
+++ src/shared/components/SidebarCompany/Sidebar.tsx
@@ -28,13 +28,13 @@ export default function Sidebar({ className }: SidebarProps) {
         className
       )}
     >
-      <BsArrowLeftShort
+      {/* <BsArrowLeftShort
         className={cn(
           "absolute -right-3 top-20 cursor-pointer rounded-full border bg-background text-3xl text-foreground",
           !isOpen && "rotate-180"
         )}
         onClick={handleToggle}
-      />
+      /> */}
       <div className="space-y-4 py-4">
         <div className="px-3 py-2">
           <div className="mt-3 space-y-1">
